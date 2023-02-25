eb9f4e02e85890c599fb2b966027d9643b7914f0
drewleean1 <drewleean@gmail.com>
2023-02-24 22:37:50 -0700

    third draft to fix index errors

--- MinPuzzle.py
+++ MinPuzzle.py
@@ -1,37 +1,85 @@
 
+import heapq
 
-def minEffort_helper(puzzle, n, m, visited):
+def minEffort_helper(puzzle, n, m, visited, result):
     current = puzzle[n][m]
-    min = 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+    #min = 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
     min_coords = (0,0)
-    for x in [(n-1, m), (n+1, m), (n, m+1), (n, m-1)]:
-        if (x[0], x[1]) in visited:
-            continue
-        elif x[0] < 0 or x[1] < 0 or x[0] > (len(puzzle[0])-1) or x[1]>(len(puzzle)-1):
-            continue
-        temporary = abs(current - puzzle[x[0]][x[1]])
-        if temporary <= min:
-            min = temporary
-            min_coords = (x[0], x[1])
-    if min_coords not in visited:
-        visited.append(min_coords)
-    return (min_coords, min)
+    if current not in visited:
+        #print(current)
+        visited.append(current)
+        #print(visited)
+        for x in [(n-1, m), (n+1, m), (n, m+1), (n, m-1)]:
+            if (x[0], x[1]) in visited:
+                continue
+            elif x[0] < 0 or x[1] < 0 or x[0] > (len(puzzle[0])-1) or x[1]>(len(puzzle)-1):
+                continue
+            temporary = abs(current - puzzle[x[0]][x[1]])
+            if temporary >= result:
+                print(result)
+                result = temporary
+                minEffort_helper(puzzle, x[0], x[1], visited, result)
 
-def minEffort(puzzle):
+    #if min_coords not in visited:
+    #    visited.append(min_coords)
+    #return (min_coords, min)
+
+def minEffort2(puzzle):
     target_n = len(puzzle[0])-1
     target_m = len(puzzle)-1
-    n = 0
-    m = 0
+    #n = 0
+    #m = 0
     result = 0
     visited = [(0,0)]
-    while m != target_m or n != target_n:
+    for n in range (len(puzzle[0])):
+        for m in range(len(puzzle)):
+            #print((n,m))
+            minEffort_helper(puzzle, n, m, visited, result)
+    '''while m != target_m or n != target_n:
         lowest_effort = minEffort_helper(puzzle, n, m, visited)
         if lowest_effort[1] > result:
             result = lowest_effort[1]
         n = lowest_effort[0][0]
-        m = lowest_effort[0][1]
+        m = lowest_effort[0][1]'''
     return result
 
-print(minEffort([[1, 3, 5], [3, 8, 3], [3, 4, 5]]))
+#print(minEffort([[1, 3, 2], [3, 7, 3], [3, 5, 6]]))
 
 #print(minEffort_helper([[1, 3, 5], [3, 8, 3], [3, 4, 5]], 0, 2, [(0,0), (0,1), (0,2)]))
+
+
+def minEffort(puzzle):
+    target_n = len(puzzle[0])
+    target_m = len(puzzle)
+    effort = {}
+    for x in range(target_n):
+        for y in range(target_m):
+            effort[x,y] = float('inf')
+    effort[0,0] = 0
+    pq = [(0, (0,0))]
+    while len (pq) > 0:
+        current_height, current_node = heapq.heappop(pq)
+        # Nodes can get added to the priority queue multiple times. We only
+        # process a vertex the first time we remove it from the priority queue.
+        if current_height > effort[current_node]:
+            continue
+        n = current_node[0]
+        m = current_node[1]
+        for x in [(n-1, m), (n+1, m), (n, m+1), (n, m-1)]:
+            print(current_node, x)
+            if x not in effort:
+                continue
+            current_effort =  abs(puzzle[n][m] - puzzle[x[0]][x[1]])
+            print('hello', current_effort)
+            # Only consider this new path if it's better than any path we've
+            # already found.
+            if current_effort < effort[x]:
+                print('yes')
+                effort[x] = current_effort
+                heapq.heappush(pq, (current_effort, x))
+
+    return effort
+
+    print(effort)
+
+print(minEffort([[1, 3, 2], [3, 7, 3], [3, 5, 6]]))
