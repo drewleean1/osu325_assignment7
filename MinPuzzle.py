cbcd3005ba546d18837a18433ff755acec345772
drewleean1 <drewleean@gmail.com>
2023-02-26 20:10:52 -0700

    attempt to fix some stuff. Make sure we can start going through list from the front

--- MinPuzzle.py
+++ MinPuzzle.py
@@ -1,6 +1,6 @@
 
 import heapq
-
+"""
 def minEffort(puzzle):
     print(puzzle)
     target_m = len(puzzle)-1
@@ -45,5 +45,38 @@ def minEffort(puzzle):
     if effort[end_of_list][0] > max_effort:
         max_effort = effort[end_of_list][0]
     return max_effort
+"""
 
-print(minEffort([[1, 3, 5], [2, 8, 3], [3, 4, 5]]))
+def minEffort(puzzle):
+    print(puzzle)
+    target_n = len(puzzle[0]) - 1
+    target_m = len(puzzle) - 1
+    effort = {}
+    for x in range(target_m + 1):
+        for y in range(target_n + 1):
+            effort[x, y] = float('inf')
+    effort[(0, 0)] = 0
+    pq = [(0, 0)]
+    visited = []
+    print(effort)
+    while len(pq) > 0:
+        current_node = heapq.heappop(pq)
+        if current_node in visited:
+            continue
+        visited.append(current_node)
+        m = current_node[0]
+        n = current_node[1]
+        for x in [(m - 1, n), (m + 1, n), (m, n + 1), (m, n - 1)]:
+            if x not in effort:
+                continue
+            if x in visited:
+                continue
+            current_effort = abs(puzzle[m][n] - puzzle[x[0]][x[1]])
+            # Only consider this new path if it's better than any path we've
+            # already found.
+            if current_effort < effort[x]:
+                effort[x] = current_effort
+                heapq.heappush(pq, (x))
+
+    return effort[(target_m, target_n)]
+
